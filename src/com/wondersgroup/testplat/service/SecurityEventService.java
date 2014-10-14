318f4889ff405d58b197e6c87503578b74d57182
叶彦波 <ye.yanbo@gmail.com>
2014-10-14 18:00:26 +0800

    新建 Rest 项目，使用了 Wink 技术

---
+++ src/com/wondersgroup/testplat/service/SecurityEventService.java
@@ -0,0 +1,33 @@
+package com.wondersgroup.testplat.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/")
+public class SecurityEventService {
+
+	@GET
+	@Path("/user")
+	@Produces(MediaType.APPLICATION_XML)
+	public User getUserInXML() {
+		return getUser("unknown");
+	}
+
+	@GET
+	@Path("/user")
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUserInJSON() {
+		return getUser("unknown");
+	}
+	
+	
+	private User getUser(String name){
+		User user = new User();
+		user.setName(name);
+		
+		return user;
+	}
+}
---
+++ src/com/wondersgroup/testplat/service/User.java
@@ -0,0 +1,30 @@
+package com.wondersgroup.testplat.service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class User {
+
+	private String name = null;
+	private int age = 20;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
